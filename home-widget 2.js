1cd3a3b79cf586a099b9dda5411af417d1068261
MrLap <98500930+MrLap@users.noreply.github.com>
2024-01-19 12:29:36 -0300

    Update home-widget 2.js

--- home-widget 2.js
+++ home-widget 2.js
@@ -7,7 +7,7 @@ import '/flutter_flow/flutter_flow_widgets.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:provider/provider.dart';
-
+//
 import 'home_model.dart';
 export 'home_model.dart';
 
